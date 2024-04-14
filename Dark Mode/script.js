b697bf470089a33fc8dc359f6e763c6fb4ce41bc
jean felippe <jeanfelippe_20@hotmail.com>
2024-04-14 15:06:27 -0300

    Exercicio de Dark Mode

---
+++ Dark Mode/script.js
@@ -0,0 +1,9 @@
+
+
+
+const $html = document.querySelector('html');
+const $checkbox = document.querySelector('#switch');
+
+$checkbox.addEventListener('change', function() {
+  $html.classList.toggle('dark-mode')
+})
